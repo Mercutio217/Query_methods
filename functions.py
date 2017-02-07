b8465ed080ec38379b7f10337844ccaea99c05fd
Lukasz <l.bielenin@gmail.com>
2017-02-07 15:43:19 +0100

    list of Wojewodztwo, Gmina, and Powiat were made

--- functions.py
+++ functions.py
@@ -1,153 +1,160 @@
 import csv
-import string
-class Powiat:
-    all_included = []
-    def __init__(self, woj_no, name, woj_name=):
-        self.woj_no = woj_no
-        if woj_name:
-        self.woj_name = "Małpolskie"
+
+class Wojewodztwo:
+    powiaty = []
+    def __init__(self, name):
         self.name = name
+        self.woj_num = 0
+
+    def __str__(self):
+        return "{} {}".format(self.woj_num, self.name)
+
+
+
+
+class Powiat:
+
+    def __init__(self, name):
+        self.name = name
+        self.woj_name = ""
+        self.pow_num = 0
+
+
+
+
+
+
+
 
     def __str__(self):
         return "Województwo {}, powiat: {}".format(self.woj_name, self.name)
 
-    @classmethod
-    def get_objects(cls, file_path):
-        file_path = csv.reader(open(str(file_path), "r"))
-        object_list = []
-        itr = 0
-        for row in file_path:
-            row = row[0].split("\t")
-            if itr == 0:
-                pass
-            else:
-                if row[5] == "powiat":
-                    woj_no = row[0]
-                    name = row[4]
+class Gmina:
 
-                    object_list.append(Powiat(woj_no, name))
-            itr = + 1
-
-        return object_list
-
-
-
-
-class District():
-    def __init__(self, county_no, dist_no, dist_name):
-        super().__init__(county_no, None)
-
-
-
-####
-
-
-class Region:
-    def __init__(self, woj, pow, gmi, rgmi, name, typ):
-        self.woj = woj
-        self.pow = pow
-        self.gmi = gmi
-        self.rgmi = rgmi
+    def __init__(self, name):
         self.name = name
-        self.type = typ
-        # self.woj = Wojewodztwo.getby_id(woj)
-
-    # def __str__(self, woj, pow, gmi, rgmi, name, typ):
-    #     return "Nr województwa: {}, Nr powiatu: {}, Nr gminy: {}, Rodzaj gminy: {}, Nazwa: {}, Typ: {}".format()
-
-    @classmethod
-    def get_objects(cls, file_path):
-        file_path = csv.reader(open(str(file_path), "r"))
-        object_list = []
-        itr = 0
-        for row in file_path:
-            if itr == 0:
-                pass
-            else:
-                row = row[0].split("\t")
-                woj = row[0]
-                pow = row[1]
-                gmi = row[2]
-                rgmi = row[3]
-                name = row[4]
-                typ = row[5]
-                object_list.append(Region(woj, pow, gmi, rgmi, name,typ))
-            itr =+ 1
-
-        return object_list
-
-
-
-class Funtions(Region):
-
-
-    @classmethod
-    def get_statistics(self, file_path):
-        stat_dict = {}
-        file_path = super().get_objects(file_path)
-        itr = 0
-        for row in file_path:
-            if itr == 1:
-                stat_dict["Title"] = row.name
-            else:
-                if str(row.type) not in stat_dict:
-                    stat_dict[row.type] = 1
-                elif str(row.type) in stat_dict:
-                    stat_dict[row.type] += 1
-
-            itr += 1
-
-        return stat_dict
-
-    @classmethod
-    def three_longest_names(self, file_path):
-        file_path = super().get_objects(file_path)
-        three_longest = {1 : "", 2 : "", 3 : ""}
-        itr = 0
-        for row in file_path:
-                if row.type == "miasto":
-                    if len(row.name) > len(three_longest[1]):
-                        three_longest[1] = row.name
-                    elif len(row.name) > len(three_longest[2]):
-                        three_longest[2] = row.name
-                    elif len(row.name) > len(three_longest[3]):
-                        three_longest[3] = row.name
-        itr += 1
-
-        return three_longest
-    @classmethod
-    def give_crowdiest_county(self, file_path):
-        file_path = super().get_objects(file_path)
-        counties_dict = {}
-        current_community = 0
-        for row in file_path:
-            if row.pow != "":
-                if row.pow not in counties_dict:
-                    counties_dict[row.pow] = 1
-                else:
-                    if current_community != row.gmi:
-                        current_community = row.gmi
-                        counties_dict[row.pow] += 1
-        biggest_key = ""
-        biggest_value = 0
-        for key, value in counties_dict.items():
-            if int(value) > int(biggest_value):
-                biggest_key = key
-                biggest_value = value
-            else:
-                pass
-
-        crowdiest_county = ""
-
-        for i in file_path:
-            if i.pow == biggest_key and i.type == "powiat":
-                crowdiest_county = "{} {}".format("powiat", i.name)
-
-        return crowdiest_county
+        self.woj = ""
+        self.powiat = ""
+        self.gmi = 0
+        self.rgmi = 0
+        self.typ = ""
 
 
 
 
-    for i in Powiat.get_objects("malopolska.csv"):
-        print(str(i))
-
+#
+#
+#
+# # class Gmina(Powiat):
+# #     def __init__(self, woj_no, name, woj_name=None):
+#
+#
+# class Region:
+#     def __init__(self, woj, pow, gmi, rgmi, name, typ):
+#         self.woj = woj
+#         self.pow = pow
+#         self.gmi = gmi
+#         self.rgmi = rgmi
+#         self.name = name
+#         self.type = typ
+#         # self.woj = Wojewodztwo.getby_id(woj)
+#
+#     # def __str__(self, woj, pow, gmi, rgmi, name, typ):
+#     #     return "Nr województwa: {}, Nr powiatu: {}, Nr gminy: {}, Rodzaj gminy: {}, Nazwa: {}, Typ: {}".format()
+#
+#     @classmethod
+#     def get_objects(cls, file_path):
+#         file_path = csv.reader(open(str(file_path), "r"))
+#         object_list = []
+#         itr = 0
+#         for row in file_path:
+#             if itr == 0:
+#                 pass
+#             else:
+#                 row = row[0].split("\t")
+#                 woj = row[0]
+#                 pow = row[1]
+#                 gmi = row[2]
+#                 rgmi = row[3]
+#                 name = row[4]
+#                 typ = row[5]
+#                 object_list.append(Region(woj, pow, gmi, rgmi, name,typ))
+#             itr =+ 1
+#
+#         return object_list
+#
+#
+#
+# class Funtions(Region):
+#
+#
+#     @classmethod
+#     def get_statistics(self, file_path):
+#         stat_dict = {}
+#         file_path = super().get_objects(file_path)
+#         itr = 0
+#         for row in file_path:
+#             if itr == 1:
+#                 stat_dict["Title"] = row.name
+#             else:
+#                 if str(row.type) not in stat_dict:
+#                     stat_dict[row.type] = 1
+#                 elif str(row.type) in stat_dict:
+#                     stat_dict[row.type] += 1
+#
+#             itr += 1
+#
+#         return stat_dict
+#
+#     @classmethod
+#     def three_longest_names(self, file_path):
+#         file_path = super().get_objects(file_path)
+#         three_longest = {1 : "", 2 : "", 3 : ""}
+#         itr = 0
+#         for row in file_path:
+#                 if row.type == "miasto":
+#                     if len(row.name) > len(three_longest[1]):
+#                         three_longest[1] = row.name
+#                     elif len(row.name) > len(three_longest[2]):
+#                         three_longest[2] = row.name
+#                     elif len(row.name) > len(three_longest[3]):
+#                         three_longest[3] = row.name
+#         itr += 1
+#
+#         return three_longest
+#     @classmethod
+#     def give_crowdiest_county(self, file_path):
+#         file_path = super().get_objects(file_path)
+#         counties_dict = {}
+#         current_community = 0
+#         for row in file_path:
+#             if row.pow != "":
+#                 if row.pow not in counties_dict:
+#                     counties_dict[row.pow] = 1
+#                 else:
+#                     if current_community != row.gmi:
+#                         current_community = row.gmi
+#                         counties_dict[row.pow] += 1
+#         biggest_key = ""
+#         biggest_value = 0
+#         for key, value in counties_dict.items():
+#             if int(value) > int(biggest_value):
+#                 biggest_key = key
+#                 biggest_value = value
+#             else:
+#                 pass
+#
+#         crowdiest_county = ""
+#
+#         for i in file_path:
+#             if i.pow == biggest_key and i.type == "powiat":
+#                 crowdiest_county = "{} {}".format("powiat", i.name)
+#
+#         return crowdiest_county
+#
+#
+#
+#
+#     for i in Powiat.get_objects("malopolska.csv"):
+#         print(str(i))
+#
--- main.py
+++ main.py
@@ -1,2 +1,49 @@
-if __name__ == __main__:
-    main()
+from functions import Wojewodztwo, Powiat, Gmina
+import csv
+
+class Main:
+    @classmethod
+    def get_objects(cls, file_path):
+        file_path = csv.reader(open(str(file_path), "r"))
+        object_list = []
+        itr = 0
+        for row in file_path:
+            row = row[0].split("\t")
+            if itr == 0:
+                pass
+            else:
+                if row[1] == "":
+                    wojewodztwo = Wojewodztwo(row[4])
+                    wojewodztwo.woj_num = row[0]
+                    object_list.append(wojewodztwo)
+                elif row[2] == "" and row[5] == "powiat":
+                    pow = Powiat(row[4])
+
+                    for object in object_list:
+                        if isinstance(object, Wojewodztwo) and row[0] == object.woj_num:
+                            pow.woj = object
+                    pow.pow_num = row[1]
+                    object_list.append(pow)
+                else:
+                    gmina = Gmina(row[4])
+                    for object in object_list:
+                        if isinstance(object, Wojewodztwo) and row[0] == object.woj_num:
+                            gmina.woj = object
+                        if isinstance(object, Powiat) and row[1] in object.pow_num:
+                            gmina.powiat = object.name
+                    gmina.typ = row[5]
+                    object_list.append(gmina)
+
+
+
+            itr =+ 1
+
+        return object_list
+
+dupa = Main.get_objects("malopolska.csv")
+print(dupa)
+for i in dupa:
+    if isinstance(i, Powiat):
+        print(i.name, i.pow_num)
+    if isinstance(i, Gmina):
+        print(i.name, i.woj, i.powiat, i.typ)
